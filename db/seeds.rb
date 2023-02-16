7efef18236c9af37f730d613ed1eda44db7b9038
DIANELLY VENTURA <DMVCode@outlook.com>
2023-02-16 15:44:40 -0500

    Seeds file update

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,6 @@
 
 # User.create(name: "Diane", email: "diane@test.com", password: "password", profile_picture: "https://avatars.githubusercontent.com/u/122197490?v=4")
 
-Board.create(user_id: 1, title: "Meditation", description: "A collection of meditation resources")
-Board.create(user_id: 1, title: "Feng shui", description: "Inspiration to feng shui your space")
-Board.create(user_id: 1, title: "Holistic Medicine", description: "Heal your mind, body and soul through holistic practices")
+# Board.create(user_id: 1, title: "Meditation", description: "A collection of meditation resources")
+# Board.create(user_id: 1, title: "Feng shui", description: "Inspiration to feng shui your space")
+# Board.create(user_id: 1, title: "Holistic Medicine", description: "Heal your mind, body and soul through holistic practices")
